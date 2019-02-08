e59470a6fca88cd2f82943f747932f97b3cea532
torrent benoit <torrentdu64@gmail.com>
2019-02-09 12:50:27 +1300

    db:migrate Girls Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190208234442) do
+ActiveRecord::Schema.define(version: 20190208234855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "girls", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "age"
+    t.bigint "shop_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_girls_on_shop_id"
+  end
 
   create_table "hours", force: :cascade do |t|
     t.string "day"
@@ -49,6 +59,7 @@ ActiveRecord::Schema.define(version: 20190208234442) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "girls", "shops"
   add_foreign_key "hours", "shops"
   add_foreign_key "shops", "users"
 end
