c6c846438821ad40a4946a2931f6a28a47d5230e
torrent benoit <torrentdu64@gmail.com>
2019-02-09 12:11:17 +1300

    db: migrate Hours Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190208225631) do
+ActiveRecord::Schema.define(version: 20190208230930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20190208225631) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["advertise_id"], name: "index_girls_on_advertise_id"
+  end
+
+  create_table "hours", force: :cascade do |t|
+    t.string "day"
+    t.time "open_hour"
+    t.time "close_hour"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
