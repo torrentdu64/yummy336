9e5cd2ef53bc7b12a3ec84c6a1ff09d1ee449381
torrent benoit <torrentdu64@gmail.com>
2019-02-11 17:48:26 +1300

    js error and scss errors stop this branch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,10 @@
+//// = require rails-ujs
+//// = require_tree .
+//// = require_default-assets
+
+
+//= require jquery
 //= require rails-ujs
+//= require bootstrap-sprockets
 //= require_tree .
+//= require_default-assets
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,14 @@
-import "bootstrap";
+ import "bootstrap";
+// import "./mona.bundle";
+// import "./default-assets/active";
+// import "./default-assets/avoid.console.error";
+// import "./default-assets/classynav";
+// import "./default-assets/jquery.scrollup.min";
+// import "./default-assets/slick.min";
+
+import 'owl.carousel2/dist/assets/owl.carousel.css';
+import $ from 'jquery';
+import 'imports?jQuery=jquery!owl.carousel';
+
+
+
