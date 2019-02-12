1c9aaa17bd827fdbe1ccdbd1590e8a32e6e73937
torrent benoit <torrentdu64@gmail.com>
2019-02-12 16:03:07 +1300

    integrate template no errors

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+ Rails.application.config.assets.precompile += %w( jquery.js bootstrap.min.js owl.carousel.min.js jquery.magnific-popup.min.js jquery.stellar.min.js smoothscroll.js wow.min.js custom.js )
