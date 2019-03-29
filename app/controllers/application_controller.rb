becaa84362de50f30a2225aa8f2c8a72942809c7
torrent benoit <torrentdu64@gmail.com>
2019-03-29 13:39:41 +1300

    meta tag to intit main controller and layout application and show girl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :set_locale
+  before_action :default_url
 
   private
 
@@ -11,6 +12,10 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
+
+  def default_url
+    { host: ENV["HOST"] || "localhost:3000" }
   end
 
 end
