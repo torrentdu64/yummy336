e456f5599b8df4a4cc12bb03c1a5b7f4839a01cb
torrent benoit <torrentdu64@gmail.com>
2019-04-01 14:40:23 +1300

    try to redeirect 301 bu no idea

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,16 @@ Rails.application.routes.draw do
       resources :shops , only: :show, path: 'brothel' do
         get :get_random_girl
         resources :girls, only: [:index, :show] , path: 'escorts' do
-          get :next
-          get :prev
+          get :next , path: ''
+          get :prev , path: ''
         end
         #resources :advertises, only: [:wechat]
         get 'job', to: 'advertises#job', :as => :job
         get 'wechat', to: 'advertises#wechat' , :as => :wechat
         get 'website', to: 'advertises#website', :as => :website
         get 'instagram', to: 'advertises#instagram', :as => :instagram
+
+
 
       end
 
@@ -39,5 +41,12 @@ Rails.application.routes.draw do
       match "/422", to: "errors#unacceptable", via: :all
       match "/500", to: "errors#internal_server_error", via: :all
 
+      get '/brothel/auckland/escort/:id', to: redirect('/brothel/auckland/escort/', status: 302)
+
+      # match '*_path' => redirect('/', status: 301) , via: :all
+      # # 301 redirect from old URLs
+      # # match "/*path/:id", to: redirect("/brothel/%{id}s"), via: :all
+      #  match "/brothel/auckland/escort/:id", to: redirect("/shops/%{id}s/girls/%{id}s", status: 302), via: :all
+      #get '/brothel/auckland/escort/:id', to: redirect('/shops/%{id}s/girls/%{id}s', status: 302)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
